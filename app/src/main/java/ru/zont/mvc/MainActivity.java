7412e5c05efdd0b3653e6b4e0013596548517583
ZONT3 <danya10001@gmail.com>
2019-02-05 20:49:18 +0300

    Reworked ObjectAdapter

--- app/src/main/java/ru/zont/mvc/MainActivity.java
+++ app/src/main/java/ru/zont/mvc/MainActivity.java
@@ -10,23 +10,75 @@ import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.GridLayoutManager;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.ProgressBar;
+import android.widget.Toast;
 
 import com.google.gson.Gson;
 
 import java.io.IOException;
 import java.net.URL;
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.HashMap;
 
 public class MainActivity extends AppCompatActivity {
+
+    private ObjectAdapter adapter;
+
+    private ProgressBar pb;
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         setSupportActionBar(findViewById(R.id.main_toolbar));
 
+        RecyclerView recyclerView = findViewById(R.id.main_recycler);
+        recyclerView.setAdapter(adapter = new ObjectAdapter());
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
 
+        pb = findViewById(R.id.main_pb);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        //TODO CheckerThreads and getList usage
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+    }
+
+    private void getList() {
+        new ListGetter(new ListGetter.AsyncRunnable() {
+            @Override
+            public void onPreExecute() {
+                pb.setVisibility(View.VISIBLE);
+            }
+
+            @Override
+            public void onPostExecute(ArtifactObject[] objects, Exception e) {
+                pb.setVisibility(View.GONE);
+                if (e != null) {
+                    Toast.makeText(MainActivity.this, e.getLocalizedMessage(), Toast.LENGTH_LONG)
+                            .show();
+                    return;
+                } else if (objects == null) {
+                    Toast.makeText(MainActivity.this, "Error on getting list", Toast.LENGTH_SHORT)
+                            .show();
+                    return;
+                }
+
+                adapter.updateDataset(objects);
+            }
+        }).execute();
     }
 
     @SuppressWarnings("CanBeFinal")
@@ -116,8 +168,14 @@ public class MainActivity extends AppCompatActivity {
     }
 
     @NonNull
-    static String getACtionString(ArtifactObject object, Context context) {
-        return ""; //TODO здесь блять сука нахуй
+    static String getActionString(ArtifactObject object, Context context) {
+        switch (object.getLastActType()) {
+            default:
+            case ArtifactObject.ACTION.CREATED: return context.getString(R.string.artobj_created);
+            case ArtifactObject.ACTION.EDITED: return context.getString(R.string.artobj_edited);
+            case ArtifactObject.ACTION.TRAINED: return context.getString(R.string.artobj_learned);
+            case ArtifactObject.ACTION.STARTED_TRAINING: return context.getString(R.string.artobj_started);
+        }
     }
 
     @NonNull
--- app/src/main/java/ru/zont/mvc/ObjectAdapter.java
+++ app/src/main/java/ru/zont/mvc/ObjectAdapter.java
@@ -9,7 +9,12 @@ import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import com.bumptech.glide.Glide;
+
+import java.text.DateFormat;
 import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Collections;
 
 class ObjectAdapter extends RecyclerView.Adapter<ObjectAdapter.VH> {
     class VH extends RecyclerView.ViewHolder {
@@ -30,9 +35,18 @@ class ObjectAdapter extends RecyclerView.Adapter<ObjectAdapter.VH> {
     }
 
     private ArrayList<ArtifactObject> dataset;
+    private OnItemClickListener listener;
+
+    ObjectAdapter() {
+        this(new ArrayList<>());
+    }
 
     ObjectAdapter(ArrayList<ArtifactObject> dataset) {
         this.dataset = dataset;
+    }
+
+    void setOnItemClickListener(OnItemClickListener listener) {
+        this.listener = listener;
     }
 
     @NonNull
@@ -50,7 +64,62 @@ class ObjectAdapter extends RecyclerView.Adapter<ObjectAdapter.VH> {
         vh.title.setText(object.getTitle());
         vh.meta.setText(context.getString(R.string.main_meta, object.getTotal()));
         vh.status.setText(MainActivity.getStatusString(object, context));
-//TODO        vh.act.setText();
+
+        Calendar calendar = Calendar.getInstance();
+        calendar.setTimeInMillis(object.getLastAct());
+        DateFormat format = DateFormat.getDateTimeInstance(DateFormat.SHORT, DateFormat.SHORT);
+        if (calendar.get(Calendar.DAY_OF_YEAR) * calendar.get(Calendar.YEAR) ==
+                Calendar.getInstance().get(Calendar.DAY_OF_YEAR) * Calendar.getInstance().get(Calendar.YEAR))
+            format = DateFormat.getTimeInstance(DateFormat.MEDIUM);
+        vh.act.setText(String.format(MainActivity.getActionString(object, context),
+                format.format(calendar.getTime())));
+
+        Glide.with(context)
+                .load(object.getThumbnail())
+                .into(vh.thumb);
+
+        vh.itemView.setOnClickListener(v -> {
+            if (listener == null) return;
+            listener.onItemClick(object);
+        });
+    }
+
+    void updateDataset(ArtifactObject[] newDataset) {
+        ArrayList<ArtifactObject> list = new ArrayList<>();
+        Collections.addAll(list, newDataset);
+        updateDataset(list);
+    }
+
+    void updateDataset(ArrayList<ArtifactObject> newDataset) {
+        for (ArtifactObject object : newDataset) {
+            if (!dataset.contains(object)) {
+                dataset.add(object);
+                notifyItemInserted(dataset.indexOf(object));
+                notifyItemRangeChanged(dataset.indexOf(object), dataset.size());
+            }
+        }
+        for (ArtifactObject object : dataset) {
+            if (!dataset.contains(object)) {
+                int pos = dataset.indexOf(object);
+                dataset.remove(object);
+                notifyItemRemoved(pos);
+                notifyItemRangeChanged(pos, dataset.size());
+            }
+        }
+        for (ArtifactObject obj : dataset) {
+            if (!obj.dataEquals(newDataset.get(newDataset.indexOf(obj)))) {
+                dataset.set(dataset.indexOf(obj), newDataset.get(newDataset.indexOf(obj)));
+                notifyItemChanged(dataset.indexOf(obj));
+            }
+        }
+    }
+
+    void clear() {
+        if (dataset.size() == 0) return;
+        int size = dataset.size();
+        dataset = new ArrayList<>();
+        notifyItemRangeRemoved(0, size);
+        notifyItemRangeChanged(0, size);
     }
 
     @Override
@@ -59,6 +128,6 @@ class ObjectAdapter extends RecyclerView.Adapter<ObjectAdapter.VH> {
     }
 
     interface OnItemClickListener {
-        void onItemClick(ArtifactObject item, int pos);
+        void onItemClick(ArtifactObject item);
     }
 }
